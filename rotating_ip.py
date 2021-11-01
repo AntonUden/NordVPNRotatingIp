ffdea05f5728c83855a8028c5f687a218008e170
Anton udén <udenanton@hotmail.com>
2021-11-01 11:58:17 +0100

    Error handling

--- rotating_ip.py
+++ rotating_ip.py
@@ -8,7 +8,11 @@ print("Somehow we managed to initialize the VPN without burning down the server 
 
 while(True):
 	print("Changing VPN server...")
-	rotate_VPN(instructions=vpn, google_check=1)
-	next_change = 60 * minutest_between_switch
-	print("Done. Next change in " + str(next_change) + " seconds")
-	time.sleep(next_change)
+	try:
+		rotate_VPN(instructions=vpn, google_check=1)
+		next_change = 60 * minutest_between_switch
+		print("Done. Next change in " + str(next_change) + " seconds")
+		time.sleep(next_change)
+	except Exception as e:
+		print("An error occured. Switching in 10 seconds")
+		time.sleep(10)
